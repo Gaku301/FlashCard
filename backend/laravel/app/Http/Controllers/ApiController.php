10bba21505e3c8167c127ccdeb442cd1b50bb049
Gaku Ishikawa <gaku.jp.301@gmail.com>
2021-01-18 21:14:41 +0900

    cURL　成功

--- backend/laravel/app/Http/Controllers/ApiController.php
+++ backend/laravel/app/Http/Controllers/ApiController.php
@@ -16,16 +16,19 @@ class ApiController extends Controller
     {
         $access_token = 'def43930c40f7ba453016f70b86d59323cd1475360d201c917fa14d44bacbd18';
         $url = 'https://boostnote.io/api/docs';
-        // $ch = curl_init($url);
-        // curl_setopt($ch, CURLOPT_HEADER, [
-        //     'Content-Type: application/json; charser=UTF-8',
-        //     'Authorization: Bearer ['.$access_token.']',
-        // ]);
+
+        // cURLセッション初期化
         $ch = curl_init();
-
+        // 転送時のオプションを設定
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer '.$access_token,
+            'Content-Type: application/x-www-form-urlencoded;charset=UTF-8',
+        ]);
+        // 転送実行後、レスポンスを取得し保存
+        $response = curl_exec($ch);
 
-        return view('index');
+        return view('index', ['response' => $response]);
     }
 }
